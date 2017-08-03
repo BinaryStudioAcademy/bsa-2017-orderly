d76075d00569766ab54066250b55bb33fc12f2ba
Client-Z <denisoleshenko@gmail.com>
2017-08-03 19:09:46 +0300

    features/server_api-team: Fix to teamSchema.

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+        id: ObjectID,
+        owner: String,
+        name: String,
+        collaborators: Array,
+        bases: Array
+});
 
 module.exports = mongoose.model('user', userSchema);
