91e0c9ce2ac659f54abaeda309f632688de205fd
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-03 22:57:10 +0300

    apply eslint rules for frontend

--- frontend/src/client/assets/javascripts/app/DevTools.js
+++ frontend/src/client/assets/javascripts/app/DevTools.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createDevTools } from 'redux-devtools';
+import {createDevTools} from 'redux-devtools';
 
 // Monitors are separate packages, and you can make a custom one
 import LogMonitor from 'redux-devtools-log-monitor';
@@ -8,10 +8,10 @@ import SliderMonitor from 'redux-slider-monitor';
 
 // createDevTools takes a monitor and produces a DevTools component
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h"
-               changePositionKey="ctrl-q"
-               changeMonitorKey="ctrl-m">
-    <LogMonitor theme="nicinabox" />
-    <SliderMonitor keyboardEnabled />
-  </DockMonitor>
+    <DockMonitor toggleVisibilityKey="ctrl-h"
+                 changePositionKey="ctrl-q"
+                 changeMonitorKey="ctrl-m">
+        <LogMonitor theme="nicinabox"/>
+        <SliderMonitor keyboardEnabled/>
+    </DockMonitor>
 )
--- frontend/src/client/assets/javascripts/app/Root.js
+++ frontend/src/client/assets/javascripts/app/Root.js
@@ -1,11 +1,9 @@
-// @flow
-
-import React, { PropTypes } from 'react';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import React, {PropTypes} from 'react';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router';
 
 import routes from './routes';
-import { SENTRY_URL } from './config';
+import {SENTRY_URL} from './config';
 
 // If you use React Router, make this component
 // render <Router> with your routes. Currently,
@@ -16,32 +14,32 @@ import { SENTRY_URL } from './config';
 
 window.Raven && Raven.config(SENTRY_URL).install();
 
-const Root = ({ store, history }) => {
-  let ComponentEl = (
-    <Provider store={store}>
-      <Router history={history} routes={routes} />
-    </Provider>
-  );
-
-  if (process.env.NODE_ENV !== 'production') {
-    const DevTools = require('./DevTools').default;
-
-    ComponentEl = (
-      <Provider store={store}>
-        <div>
-          <Router history={history} routes={routes} />
-          {!window.devToolsExtension ? <DevTools /> : null}
-        </div>
-      </Provider>
+const Root = ({store, history}) => {
+    let ComponentEl = (
+        <Provider store={store}>
+            <Router history={history} routes={routes}/>
+        </Provider>
     );
-  }
 
-  return ComponentEl;
+    if (process.env.NODE_ENV !== 'production') {
+        const DevTools = require('./DevTools').default;
+
+        ComponentEl = (
+            <Provider store={store}>
+                <div>
+                    <Router history={history} routes={routes}/>
+                    {!window.devToolsExtension ? <DevTools/> : null}
+                </div>
+            </Provider>
+        );
+    }
+
+    return ComponentEl;
 };
 
 Root.propTypes = {
-  history: PropTypes.object.isRequired,
-  store: PropTypes.object.isRequired
+    history: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
 };
 
 export default Root;
