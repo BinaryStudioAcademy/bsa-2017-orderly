3b7d453071b620224ffba72b63c8a29f3e3f019f
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-03 23:14:13 +0300

    apply eslint rules for backend

--- backend/server.js
+++ backend/server.js
@@ -11,26 +11,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 require('./routes/index')(router);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 });
 
-app.use("./api", router)
+app.use("./api", router);
 
 app.use((req, res, next) => {
-	let err = new Error('Route not Found');
-	err.status = 404;
-	next(err);
+    let err = new Error('Route not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.use((err, req, res) => {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	res.status(err.status || 500);
-	res.render('error');
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(2020, () => {
-	console.log('Server is started on localhost:2020');
+    console.log('Server is started on localhost:2020');
 });
 
 module.exports = app;
