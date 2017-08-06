a41b453a00d5bbd176f9d7163dac05c16a42f43b
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-06 13:46:19 +0300

    frontend-preparation: basic structure ready

--- frontend/src/javascripts/app/routes.js
+++ frontend/src/javascripts/app/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Route, Redirect} from 'react-router';
 
 import App from './App';
-import NotFoundView from '../components/NotFound';
+import NotFound from '../components/notFound/notFound';
 
 export default (
     <Route path="/" component={App}>
-        <Route path="404" component={NotFoundView}/>
+        <Route path="404" component={NotFound}/>
         <Redirect from="*" to="404"/>
     </Route>
 );
