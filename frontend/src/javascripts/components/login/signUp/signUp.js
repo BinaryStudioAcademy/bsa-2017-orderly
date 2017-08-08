e76310c4126b2937ee30ccafe62e9bf25d9b1fc0
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-09 01:49:22 +0300

    feature/Login_Pages_auth_sign-up: can not see scss styles

--- frontend/src/javascripts/components/login/signUp/signUp.js
+++ frontend/src/javascripts/components/login/signUp/signUp.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import {Container, Header} from 'semantic-ui-react';
+import {Container, Header, Segment} from 'semantic-ui-react';
 import './signUp.scss';
 
 class SignUp extends Component {
     render() {
         return (
             <Container textAlign="center">
-                <Header as="h1" color="blue" className="sign-up__header">Orderly</Header>
+                <Header as="h1" color="blue">Orderly</Header>
+                <Container className="sign-up_form">
+                    <Segment basic>Create account</Segment>
+                </Container>
             </Container>
         );
     }
