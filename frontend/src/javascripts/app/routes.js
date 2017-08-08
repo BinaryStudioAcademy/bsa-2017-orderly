b460b4254cb880a376c363ee13afee852c6fc672
chulanovskyi <skyrocker@mail.ru>
2017-08-08 20:29:38 +0300

    start working on main container

--- frontend/src/javascripts/app/routes.js
+++ frontend/src/javascripts/app/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Route, Redirect, IndexRoute} from 'react-router';
-
+import {Route, IndexRoute} from 'react-router';
 import App from './App';
 import StartPage from '../components/StartPage';
 import NotFound from '../components/notFound/notFound';
+import SignUp from '../components/login/signUp/signUp';
 
 export default (
     <Route path="/" component={App}>
-      <IndexRoute component={StartPage}/>
+        <IndexRoute component={StartPage}/>
         <Route path="404" component={NotFound}/>
-        <Redirect from="*" to="404"/>
+        <Route path="signup" component={SignUp}/>
     </Route>
 );
--- frontend/src/javascripts/components/login/signUp/signUp.js
+++ frontend/src/javascripts/components/login/signUp/signUp.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Container, Header} from 'semantic-ui-react';
+import './signUp.scss';
+
+class SignUp extends Component {
+    render() {
+        return (
+            <Container textAlign="center">
+                <Header as="h1" color="blue" className="sign-up__header">Orderly</Header>
+            </Container>
+        );
+    }
+}
+
+export default SignUp;
