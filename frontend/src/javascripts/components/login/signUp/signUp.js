c0d70b5dda8284baff6c399381807d1cfd60c624
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-09 16:11:17 +0300

    feature/Login_Pages_auth_sign-up: markup ready

--- frontend/src/javascripts/components/login/signUp/signUp.js
+++ frontend/src/javascripts/components/login/signUp/signUp.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-import {Container, Header, Segment} from 'semantic-ui-react';
+import {Container, Header, Form, Input, Button, Item} from 'semantic-ui-react';
 import './signUp.scss';
 
 class SignUp extends Component {
     render() {
         return (
-            <Container textAlign="center">
+            <Container text textAlign="center" id="sign-up__container">
                 <Header as="h1" color="blue">Orderly</Header>
-                <Container className="sign-up_form">
-                    <Segment basic>Create account</Segment>
+                <Container text id="sign-up__form">
+                    <Item>Create an account</Item>
+                    <Form>
+                        <Form.Group widths='equal'>
+                            <Form.Field control={Input} label='First name' placeholder='First name' />
+                            <Form.Field control={Input} label='Last name' placeholder='Last name' />
+                        </Form.Group>
+                        <Form.Field control={Input} label='Email' placeholder='Email' />
+                        <Form.Field control={Input} label='Password' placeholder='Password' />
+                        <Form.Field fluid control={Button} content='Sign Up' id='form__confirm'/>
+                    </Form>
+                </Container>
+                <Container id='sign-up__google'>
+                    <Button fluid content='Sign up with Google'/>
+                </Container>
+                <Container textAlign="left" id='sign-up__footer'>
+                    <p>Already have an account?</p>
+                    <a href="/signin">Sign In</a>
                 </Container>
             </Container>
         );
