bad98e0d533fb09170711563954efb0e3f7de0df
chulanovskyi <skyrocker@mail.ru>
2017-08-11 00:47:58 +0300

    working

--- frontend/src/javascripts/components/login/signUp/signUpApi.js
+++ frontend/src/javascripts/components/login/signUp/signUpApi.js
@@ -1,9 +1,16 @@
 function callAddUserApi(credentials) {
     console.log('CALL ADD USER API');
     console.log(credentials);
-    const url = 'http://localhost:3000/user/signup';
-    return fetch(url, {method: 'POST'})
-        .then((response) => response.json().then((json) => ({json, response})));
+    const url = 'http://localhost:2020/api/user';
+    const headers = new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+    });
+    return fetch(url, {method: 'POST', headers: headers, body: JSON.stringify(credentials)})
+        .then((response) => {
+            console.log(response);
+            return response;
+        });
     //     .then(
     //         (response) => ({response}),
     //         (error) => ({error: error.message})
--- frontend/src/javascripts/components/login/signUp/signUpSaga.js
+++ frontend/src/javascripts/components/login/signUp/signUpSaga.js
@@ -4,9 +4,10 @@ import { SIGN_UP_ACTION } from "./signUpActions";
 
 // worker Saga: will be fired on GET_USER_NAME_REQUESTED actions
 function* fetchUser(action) {
+    console.log(action);
     try {
-        const credentials = yield call(signUpApi.fetchSignUp, action.credentials);
-        yield put({type: SIGN_UP_ACTION, credentials: credentials});
+        const credentials = yield call(signUpApi.fetchSignUp, action);
+        yield put({type: "SIGN_UP_SUCCESS", credentials: credentials});
     } catch (e) {
         console.log(e);
         yield put({type: "SIGN_UP_FAILED", message: e.message});
