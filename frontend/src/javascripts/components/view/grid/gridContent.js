099eb00fa6bb458b9751eae76392930df934bfad
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-13 23:03:32 +0300

    figuring out how to resize table col

--- frontend/src/javascripts/components/view/grid/gridContent.js
+++ frontend/src/javascripts/components/view/grid/gridContent.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import {Container} from 'semantic-ui-react';
+import './grid.scss';
 
 export const GridContent = () => {
     return (
         <Container fluid>
-            <h1>View content</h1>
+            <table id="content__grid">
+                <thead>
+                <tr>
+                    <th id="grid__row">
+                        <input type="checkbox"/>
+                    </th>
+                    <th>Name</th>
+                    <th>Field</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>First</td>
+                    <td>Custom data</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Second</td>
+                    <td>Custom data</td>
+                </tr>
+                </tbody>
+            </table>
         </Container>
     );
 };
--- frontend/src/javascripts/components/view/grid/gridHeader.js
+++ frontend/src/javascripts/components/view/grid/gridHeader.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import {Container, Icon} from 'semantic-ui-react';
+import {Icon, Button} from 'semantic-ui-react';
+import {viewIcons} from '../../configuration/viewTypes';
+import './grid.scss';
 
 export const GridHeader = () => {
     return (
-        <Container fluid>
-            <div className="header__view-selector">
-            <Icon name="caret down"/>
+        <div className="view__header">
+                <Icon name="caret down" size="large"/>
+            <div id="header__view-type">
+                <Icon name={viewIcons.grid} size="large"/>
+                Grid View
             </div>
-            <div className="header__current-view">
-                Current View
-            </div>
-        </Container>
+            <Button.Group>
+                <Button basic>
+                    <Icon name='hide'/>
+                    <span className="menu__text">Hide fields</span>
+                </Button>
+                <Button basic>
+                    <Icon name='filter'/>
+                    <span className="menu__text">Filter</span>
+                </Button>
+                <Button basic>
+                    <Icon name='browser'/>
+                    <span className="menu__text">Group</span>
+                </Button>
+                <Button basic>
+                    <Icon name='sort content ascending'/>
+                    <span className="menu__text">Sort</span>
+                </Button>
+                <Button basic icon='external'/>
+                <Button basic icon='ellipsis horizontal'/>
+            </Button.Group>
+            <Icon name="search" id="header__search"/>
+        </div>
     );
 };
