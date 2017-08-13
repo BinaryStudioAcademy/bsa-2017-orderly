57dd926a258ab724e3c6383466f2d655136fd6ae
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-14 01:40:40 +0300

    feature/View_Table: collector for header and content

---
+++ frontend/src/javascripts/components/view/grid/grid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {GridHeader} from './gridHeader';
+import {GridContent} from './gridContent';
+
+export const Grid = () => {
+    return (
+        <div>
+            <GridHeader/>
+            <GridContent/>
+        </div>
+    );
+};
