dd3d8a5206b535f09decb864e03da2ad4d26e333
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-16 00:26:05 +0300

    feature/View_Table: add toggle selector action

--- frontend/src/javascripts/components/view/viewActions.js
+++ frontend/src/javascripts/components/view/viewActions.js
@@ -1,4 +1,11 @@
 export const CHANGE_VIEW = 'CHANGE_VIEW';
+export const TOGGLE_SELECTOR = 'TOGGLE_SELECTOR';
+
+export function toggleSelector() {
+    return {
+        type: TOGGLE_SELECTOR
+    };
+}
 
 export function changeView(viewId) {
     return {
