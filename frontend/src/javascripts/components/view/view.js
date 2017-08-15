21f2c5ac8abc6941a4bf1ac7da449f2f74223efa
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-16 00:25:25 +0300

    feature/View_Table: mock views data, view selector ready

--- frontend/src/javascripts/components/view/view.js
+++ frontend/src/javascripts/components/view/view.js
@@ -2,18 +2,26 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as viewActions from './viewActions';
+import {Icon} from 'semantic-ui-react';
 import {Grid} from './grid/grid';
+import {viewIcons} from '../configuration/viewTypes';
+import './view.scss';
 
 class View extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            currentView: '',
-        };
     }
 
+    handleToggleSelector = () => {
+        this.props.toggleSelector();
+    };
+
+    handleChangeView = (id) => {
+        this.props.changeView(id);
+    };
+
     viewSelector(listOfViews) {
-        const activeView = listOfViews.filter((view) => view.isActive);
+        const activeView = listOfViews.filter((v) => v.id === this.props.view.currentView);
         switch (activeView.type) {
         case 'grid':
             return <Grid/>;
@@ -22,15 +30,41 @@ class View extends Component {
         }
     }
 
-    //Need to receive list of views from outer reducer
+    //Need to receive list of views from table reducer
     render() {
-        return this.viewSelector([{isActive: true, type: 'grid'}]);
+        return (
+            <div>
+                <Icon name="caret down"
+                      id="header__caret"
+                      size="large"
+                      onClick={this.handleToggleSelector}/>
+                <div className={this.props.view.showSelector ? 'view__selector' : 'hide'}>
+                    {this.props.view.views.map((view, ind) => {
+                        return (
+                            <div key={ind}
+                                 className="selector__option"
+                                 onClick={() => this.handleChangeView(view.id)}>
+
+                                <Icon
+                                    name="checkmark"
+                                    className={view.id === this.props.view.currentView
+                                        ? '' : 'option__notActive'}/>
+                                <Icon name={viewIcons[view.type]}/>
+                                {view.name}
+                            </div>)
+                    })
+                    }
+                </div>
+                {this.viewSelector(this.props.view.views)}
+            </div>
+        );
     }
 }
 
 function mapStateToProps(state) {
     return {
-        dashboardState: state.dashboardReducer
+        view: state.view,
+        dashboardState: state.dashboardReducer,
     };
 }
 
