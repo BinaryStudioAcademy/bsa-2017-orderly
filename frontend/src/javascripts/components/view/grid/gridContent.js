a81317f52c3b93d5c047f03ae01bd75f8c264f28
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-16 14:37:55 +0300

    feature/View_Table: test with mocked fields

--- frontend/src/javascripts/components/view/grid/gridContent.js
+++ frontend/src/javascripts/components/view/grid/gridContent.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import './gridContent.scss';
 import {Icon} from 'semantic-ui-react';
 import {GridCell} from './gridCell';
 
-export class GridContent extends Component {
+const MOCK_FIELDS = [
+    {
+        description: "Gadgets without a single problem",
+        type: "text",
+        name: "Xiaomi",
+        _id: "5992e76c73640e30de3a511b"
+    },
+    {
+        description: "All type of gadgets",
+        type: "text",
+        name: "Samsung",
+        _id: "5994223abab5206884398fe1"
+    }
+];
+
+const Field = ({type, name}) => {
+    return (
+        <div className="header__item">
+            <Icon name="font" className="item__icon"/>
+            <span>{name}</span>
+        </div>
+    );
+};
+
+export default class GridContent extends Component {
+    constructor(props){
+        super(props);
+        this.props = props
+    }
+
+    handleAddField = () => {
+
+    };
+
     render() {
         return (
             <div className="grid__content">
@@ -15,20 +49,17 @@ export class GridContent extends Component {
                         <Icon name="font" className="item__icon"/>
                         <span>Primary</span>
                     </div>
-                    <div className="header__item">
-                        <Icon name="font" className="item__icon"/>
-                        <span>First</span>
-                    </div>
-                    <div className="header__item">
-                        <Icon name="font" className="item__icon"/>
-                        <span>Second</span>
+                    {this.props.currentTable &&
+                    this.props.currentTable.fields.map((field) => {
+                        return <Field key={field._id} name={field.name}/>
+                    })}
+                    <div className="header__item item__add-field" onClick={this.handleAddField}>
+                        <Icon name="plus" className="item__icon"/>
                     </div>
                 </div>
                 <div className="content__body">
                     <div className="body__row">
-                        <div className="body__item item__row-selector">
-                            1
-                        </div>
+                        <div className="body__item item__row-selector">1</div>
                         <GridCell type='text' data='Primary Field'/>
                         <GridCell type='text' data='One'/>
                         <GridCell type='text' data='Two'/>
@@ -38,3 +69,10 @@ export class GridContent extends Component {
         );
     }
 }
+
+// function mapStateToProps(state) {
+//     return {
+//         gridContent: state,
+//     };
+// }
+// export default connect(mapStateToProps)(GridContent);
