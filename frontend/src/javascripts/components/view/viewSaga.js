188afcf6b55e8ad8a786920ae3919bed5b29b9d6
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-16 14:42:28 +0300

    feature/View_Table: remove unnecessary method 'getTableFields'

--- frontend/src/javascripts/components/view/viewSaga.js
+++ frontend/src/javascripts/components/view/viewSaga.js
@@ -1,17 +1,26 @@
 import {call, put, takeEvery} from 'redux-saga/effects';
-import {ADD_COLUMN} from "./grid/gridActions";
-import {addColumn} from './viewApi';
+import {ADD_FIELD} from "./viewActions";
+import {addFieldApi} from './viewApi';
 
-function* addNewColumn(action) {
+// function* getTableFields(action) {
+//     try {
+//         const fields = yield call(getFieldsApi, action.tableId);
+//         console.log(fields);
+//     } catch (err) {
+//         yield put({type: 'GET_FIELDS_FAILED', message: err.message});
+//     }
+// }
+
+function* addNewField(action) {
     try {
-        const result = yield call(addColumn, action._id);
+        const result = yield call(addFieldApi, action._id);
     } catch (err) {
         yield put({type: 'ADD_COLUMN_FAILED', message: err.message});
     }
 }
 
 function* viewSaga() {
-    yield takeEvery(ADD_COLUMN, addNewColumn);
+    yield takeEvery(ADD_FIELD, addNewField);
 }
 
 export default viewSaga;
