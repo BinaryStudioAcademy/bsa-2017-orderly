03739672f367b7de22a05d532fbe60277d470560
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-08-16 18:15:02 +0300

    fix bug with input in context-menu

--- frontend/src/javascripts/components/homePage/homePageBase/homePageBaseItem.js
+++ frontend/src/javascripts/components/homePage/homePageBase/homePageBaseItem.js
@@ -11,16 +11,16 @@ class BaseItem extends Component {
   render() {
     return (
       <div>
-        <div className='base-name'>
+        <div className='base-name-header'>
           {this.props.base.name}
         </div>
         <div className = 'one-base-wrapper' >
         <div className = 'one-base' style = {{backgroundColor: `${this.props.base.color}` }} > 
           <div className = 'one-base-icon'>
             <Link to={`/dashboard/${this.props.base._id}`}>
-            <Icon inverted link  size='huge' name={this.props.base.icon} 
+              <Icon inverted link  size='huge' name={this.props.base.icon} 
               />
-              </Link>
+            </Link>
             </div>
             <div> 
               <div>
