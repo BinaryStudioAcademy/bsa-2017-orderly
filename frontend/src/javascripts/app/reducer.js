4ee854b165ca1672e43d99fa22005b8c2d2d2d63
SergiyKistenyuk <sergiy.kistenyuk@gmail.com>
2017-08-19 15:43:32 +0300

    Initial commit

--- frontend/src/javascripts/app/reducer.js
+++ frontend/src/javascripts/app/reducer.js
@@ -7,7 +7,6 @@ import userProfile from '../components/userProfile/userProfileReducer';
 import baseStore from '../components/homePage/homePageReducer';
 import view from '../components/view/viewReducer';
 import grid from '../components/view/grid/gridReducer';
-import expandRecord from '../components/view/expandRecord/expandRecordReducer';
 
 export default combineReducers({
     routing,
--- frontend/src/javascripts/components/dashboard/tools/header/header.js
+++ frontend/src/javascripts/components/dashboard/tools/header/header.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Icon, Image, Label } from 'semantic-ui-react';
+import { Icon, Label } from 'semantic-ui-react';
 import { browserHistory } from 'react-router';
 import UserProfile from '../../../userProfile/userProfile';
-import avatar from '../../../../../images/avatar.png';
 import './header.scss';
 
 const Header = ({ base, user }) => (
     <header className='dashboard_header' style={{backgroundColor:`${base.color}`}}>
         <Icon link name={base.icon}
               onClick={() => {
-              	browserHistory.push('/')
+                  browserHistory.push('/')
               }}
               size='big'/>
         <div className='label'>
             <Label as='a'
-                size='big'
-                >{base.name}</Label>
+                   size='big'
+            >{base.name}</Label>
         </div>
         <div className='info'>
             <Icon link name='help circle' size='large'/>
