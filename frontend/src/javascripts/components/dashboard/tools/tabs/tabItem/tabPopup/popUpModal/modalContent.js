5deec06ce549122513df76a6d524248022141ac3
Alex <bloodhound1982@gmail.com>
2017-08-19 18:38:55 +0300

    add functional for editing description of table, saving cell by onBlur

--- frontend/src/javascripts/components/dashboard/tools/tabs/tabItem/tabPopup/popUpModal/modalContent.js
+++ frontend/src/javascripts/components/dashboard/tools/tabs/tabItem/tabPopup/popUpModal/modalContent.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Modal, Input } from 'semantic-ui-react';
 import R from 'ramda';
+import { debounce } from 'throttle-debounce';
 
-import { debounce } from '../../../../../dashboardService';
 let renameInput;
+let descriptionInput;
 
-const checkValidName = debounce((value, tablesNames, checkRenameFunc) => {
+const checkValidName = debounce(150, (value, tablesNames, checkRenameFunc) => {
 	if (!value || R.contains(R.toLower(value), tablesNames)) checkRenameFunc(true);
 	else checkRenameFunc(false);
-}, 150);
+});
 
-const ModalBody = ({activeModal, renameIsError, tablesNames, checkTableName}) => {
+const ModalBody = ({activeModal, renameIsError, tablesNames, checkTableName, table}) => {
+	renameInput = table.name;
+	descriptionInput = table.description;
 	switch (activeModal) {
 		case 'rename': return (<Input error={renameIsError} fluid
 			              onChange={(event) => {
 				              renameInput = event.target.value;
 				              checkValidName(renameInput, tablesNames, checkTableName);
 			              }}
-			              placeholder='enter new name...'/>)
-		case 'edit description': return (<Input error={renameIsError} fluid
-		                                        onChange={(event) => {
-			                                        renameInput = event.target.value;
-			                                        console.log(renameInput)
-		                                        }}
-		                                        placeholder='enter new description...'/>)
+			              placeholder={renameInput}/>)
+		case 'edit description': return (<Input fluid
+		                                        onChange={(event) => { descriptionInput = event.target.value; }}
+		                                        placeholder={descriptionInput ? descriptionInput : 'Enter description...'}/>)
 		case 'duplicate': return (<Modal.Content>
 									<p>Are you sure you want to duplicate this table?</p>
 								</Modal.Content>)
@@ -33,10 +33,8 @@ const ModalBody = ({activeModal, renameIsError, tablesNames, checkTableName}) =>
 		default: return (<div></div>)
 }}
 
-
-
-
 export {
 	ModalBody,
-	renameInput
+	renameInput,
+	descriptionInput
 };
