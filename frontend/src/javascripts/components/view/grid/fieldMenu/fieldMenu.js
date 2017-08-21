ef077af0487ce9c48a1320090c4d711fcd45f2c1
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-22 00:34:10 +0300

    feature/Change_field_type: delete field icon

--- frontend/src/javascripts/components/view/grid/fieldMenu/fieldMenu.js
+++ frontend/src/javascripts/components/view/grid/fieldMenu/fieldMenu.js
@@ -87,6 +87,7 @@ export default class FieldMenu extends Component {
                            onChange={this.handleChangeName}
                            onKeyPress={this.handleKeyPress}
                     />
+                    <Icon name="trash outline" id="menu__delete" size="large"/>
                     <FieldOptions id={this.props.id}
                                   excludeType={this.props.type}
                                   changeFieldType={this.props.changeFieldType}
