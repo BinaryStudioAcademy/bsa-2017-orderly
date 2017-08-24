9b54bcb91cb108100b83919b5d6de286ebdedbd9
Chulanovskyi <skyrocker2013@gmail.com>
2017-08-25 01:38:07 +0300

    feature/Field_email: add validation

--- frontend/src/javascripts/components/view/grid/fields/email/email.js
+++ frontend/src/javascripts/components/view/grid/fields/email/email.js
@@ -9,6 +9,11 @@ class Email extends Field {
         super(props, 'email');
     }
 
+    validateEmail(email) {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
     renderActiveField() {
         return <Input
             onBlur={(event) => this.props.onBlurComponent(this.props.id, event.target.value)}
@@ -19,8 +24,13 @@ class Email extends Field {
     }
 
     renderSelectedField() {
-        return <div className='email' onClick={() => window.open(`mailto:${this.props.value}`, '_self')}>
-                   {this.props.value}
+        let mail = this.props.value;
+        return <div className='email' onClick={() => {
+            if (this.validateEmail(mail)) {
+                return window.open(`mailto:${mail}`, '_self');
+            }
+        }}>
+                   {mail}
                </div>;
     }
 }
