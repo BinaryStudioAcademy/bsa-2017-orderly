6c4851b8546b40be5f0b6aea5e8c9c08ff9cb87f
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-08-30 20:46:46 +0300

    done percentage input

--- frontend/src/javascripts/components/view/grid/fields/percent/percent.js
+++ frontend/src/javascripts/components/view/grid/fields/percent/percent.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
-import NumericInput from 'react-numeric-input';
+import NumberFormat from 'react-number-format';
 import Field from '../field';
 import './percent.scss';
 
 class Percent extends Field {
-    constructor(props){
-        super(props, 'text-line');
-    }
-
-    // handleChange(event, maskedvalue, floatvalue){
-    //     this.setState({amount: maskedvalue});
-    // }
-
-    renderActiveField() {
-        return  (
-            <NumericInput 
-                precision="2" 
-                className="percent-input"
-                onBlur={(event) => this.props.onBlurComponent(this.props.id, event.target.value)}
-                autoFocus={true}
-            />
-        )
+  constructor(props) {
+    super(props);
+    this.state = { 
+        percent: '' 
     }
 }
+  
+  renderActiveField() {
+    return (
+        <NumberFormat className='percent-input' 
+            thousandSeparator={true} suffix={'%'} 
+            value={this.state.percent}
+            onChange={(event) => this.setState({percent: event.target.value})}
+            decimalPrecision={1}
+            onBlur={(event) => this.props.onBlurComponent(this.props.id, event.target.value)}
+            autoFocus={true}
+        />
+    )
+  }  
+}  
 
 export default Percent;
