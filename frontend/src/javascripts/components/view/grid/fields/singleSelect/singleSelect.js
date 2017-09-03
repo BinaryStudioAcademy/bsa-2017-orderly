0238fc6e27fc8e68a18209d7db17f9ec57a77730
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-09-03 22:02:17 +0300

    fix styles

--- frontend/src/javascripts/components/view/grid/fields/singleSelect/singleSelect.js
+++ frontend/src/javascripts/components/view/grid/fields/singleSelect/singleSelect.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import Select from 'react-select';
 import './singleSelect.scss';
 import Field from '../field';
 
 class SingleSelect  extends Field {
   constructor(props) {
-    super(props);
+        super(props);
+        this.state = {
+            options:[],
+            value:'',
+            label:''
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        let options = [];
+        let propsOptions=this.props.currentField.options.select
+        let i = 0;
+        for (let option in propsOptions) {
+        options.push({
+                value: i++, 
+                label: propsOptions[option],
+            })
+        }
+        this.setState({ 
+            options: options
+        });
     }
     renderActiveField() {
         return (
-            <select className='single-select-container'
-                onBlur={(event) => this.props.onBlurComponent(this.props.id, event.target.value)}
-                autoFocus={true}
-            >
-                {this.props.currentField.options.select.map((option, i) => {
-                    return ( 
-                    <option key={i}>{option}</option>
-                    )})}
-            </select>
+            <div className='single-select-container'>
+                <Select options={this.state.options}
+                    value={this.state.value}
+                    onChange = {(event) => {
+                        this.setState({value: event.value, label: event.label})}}
+                    onBlur={(event) => this.props.onBlurComponent(this.props.id, this.state.label)}
+                    autoFocus={true}
+                />
+            </div>
 
         )
     }
