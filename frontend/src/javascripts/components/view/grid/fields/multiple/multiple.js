03bbeb1b91ece97f3cbb32ea8526f53657bdb449
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-09-06 17:30:37 +0300

    multiple select problem

--- frontend/src/javascripts/components/view/grid/fields/multiple/multiple.js
+++ frontend/src/javascripts/components/view/grid/fields/multiple/multiple.js
@@ -8,42 +8,44 @@ class MultipleSelect  extends Field {
         super(props, 'multiple');
         let options = [];
         let propsOptions=this.props.currentField.options.multiple;
-        let i = 0;
+
         for (let option in propsOptions) {
         options.push({
-                value: i++, 
+                value: option++, 
                 label: propsOptions[option],
             })
         }
-            
-        let indexes = []
-        let newValues = this.props.value.split(',');
 
-        for (let j in newValues) {
-            indexes[j] = propsOptions.indexOf(newValues[j])
-        }
-        console.log(this.props.value)
-        console.log(indexes)
+        // let newValues =[]
+        // let k=0
+        // let values = this.props.value.split(',')
+
+        // for ( let j in values ) {
+        //      newValues.push({
+        //         value: k++, 
+        //         label: values[j],
+        //     })
+        // }
+//if you place newValues into valueSelected, in expand records you can not edit the record (but the current selected value will be there)
+//if you do not place newValues into valueSelected, then you can edit the record but no preselected value in the input    
         this.state = {
-            options:options,
-            value: indexes,
+            options: options,
+            valueSelected:[],
             label:[]
         }
     }
 
-    componentWillReceiveProps(nextProps, nextState) {
-        let options2 = [];
-        let propsOptions2=nextProps.currentField.options.multiple;
-        let i = 0;
-        for (let option in propsOptions2) {
-        options2.push({
-                value: i++, 
-                label: propsOptions2[option],
+    componentWillReceiveProps(nextProps) {
+        let options = [];
+        let propsOptions=this.props.currentField.options.multiple;
+        for (let option in propsOptions) {
+        options.push({
+                value: option++, 
+                label: propsOptions[option],
             })
         }
-        
         this.setState({ 
-            options: options2
+            options: options
         });
     }
 
@@ -52,17 +54,14 @@ class MultipleSelect  extends Field {
             <div className='multiple-select-container'>
                 <Select options={this.state.options}
                     multi={true}
-                    value={this.state.value}
+                    value={this.state.valueSelected}
                     onChange = {(event) => {
                         let labelArr=[];
                         for ( let i in event) {
                             labelArr[i]=event[i].label;
-                        } 
-                        let valueArr=[...this.state.value];
-                        for ( let i in event) {
-                            valueArr[i]=event[i].value;
-                        } 
-                        this.setState({value: valueArr, label: labelArr})}}
+                        }
+                        console.log(event)
+                        this.setState({valueSelected: event, label: labelArr})}}
                     onBlur={(event) => this.props.onBlurComponent(this.props.id, this.state.label)}
                     autoFocus={true}
                 />
