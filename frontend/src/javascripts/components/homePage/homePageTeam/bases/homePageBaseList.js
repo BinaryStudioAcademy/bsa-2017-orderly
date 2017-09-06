24384ba65478a519d65f77c9e5754c85d8c3b9f7
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-09-06 10:12:15 +0300

    done visual part

--- frontend/src/javascripts/components/homePage/homePageTeam/bases/homePageBaseList.js
+++ frontend/src/javascripts/components/homePage/homePageTeam/bases/homePageBaseList.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
 import BaseItem from './homePageBaseItem';
+import template from '../../../../../images/template_gallery_icon.png'
+import importSpreadsheet from '../../../../../images/upload_icon.png'
+import scratch from '../../../../../images/empty_template_icon.png'
 
 let temporaryKey = 0;
 
 class BaseList extends Component {
 	constructor(props) {
 		super(props)
-
+		this.state ={
+			showMewnu: false
+		}
+		this.handleClickOnMenu = this.handleClickOnMenu.bind(this);
+    	this.handleOutsideClick = this.handleOutsideClick.bind(this);
 	}
+  	handleClickOnMenu(event) {
+	    if (!this.state.showMewnu) {
+	      document.addEventListener('click', this.handleOutsideClick, false);
+	    } else {
+	      document.removeEventListener('click', this.handleOutsideClick, false);
+	    }
 
+	     if (this.refs.createBase) {
+	      this.setState(prevState => ({
+	         showMewnu: !prevState.showMewnu
+	      }));
+	    }
+  	}
+	handleOutsideClick(e) {
+	    if ( event.target.closest(".addBasePopover") === null) {
+	      if (this.node) {
+	        if (this.node.contains(e.target)) {
+	          return;
+	        }
+	      }
+	    this.handleClickOnMenu();
+	    }
+  	}
 	render() {
 		const props = this.props
 		if (this.props.bases) {
@@ -26,7 +55,27 @@ class BaseList extends Component {
 						)
 					})
 					}
-					<div className='btn-add-base' onClick={() => props.onNewBaseClick('#234FED', props.teamId)}>+</div>
+					<div className='relative'>
+						<div className='btn-add-base' onClick={(event) => this.handleClickOnMenu(event)}>+</div>
+						<div ref="createBase" className ={
+								this.state.showMewnu ? "addBasePopover" : "none"} >
+							<div>
+								<div className='messageTip'></div>
+								<div className='options'> 
+									<img src={template} />
+									<div className='text'>Start with a template</div>
+								</div>
+								<div className='options'> 
+									<img src={importSpreadsheet} />
+									<div className='text'>Import a spreadsheet</div>
+								</div>
+								<div className='options' onClick={(event) => {props.onNewBaseClick('#234FED', props.teamId); this.handleClickOnMenu(event)}}> 
+									<img src={scratch} />
+									<div className='text'>Start from scratch</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			)
 		} else {
