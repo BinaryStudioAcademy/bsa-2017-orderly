7015ef68af96369ebe0f8f43841eb00f16f89cbf
Alex <bloodhound1982@gmail.com>
2017-09-10 17:57:46 +0300

    arrange tasks by kanban-stack

--- frontend/src/javascripts/components/view/kanban/kanbanViewContainer/kanbanViewContainer.js
+++ frontend/src/javascripts/components/view/kanban/kanbanViewContainer/kanbanViewContainer.js
@@ -5,6 +5,8 @@ import R from 'ramda'
 import StackItem from './stackItem/stackItem'
 import './kanbanViewContainer.scss'
 
+let stackCounter = 0
+
 const mapIndexed = R.addIndex(R.map)
 
 const createFieldStackObj = (field, idx) => {
@@ -24,14 +26,16 @@ class KanbanViewContainer extends Component {
 	render() {
 		const fieldIdStacked = R.compose(R.prop('field'), defaultToZero, R.find(R.prop('isStacked'))) (this.props.view.fields_config)
 		const stackField = R.compose(R.find(field => field.fieldObj._id === fieldIdStacked), mapIndexed(createFieldStackObj))(this.props.table.fields || [])
-		console.log(stackField, 'viewdfdfdfdfddf')
 		return (
 			<div className='kanban_workflow'>
 				<div className='kanban_stack'>
 					<StackItem currentViewId={this.props.currentViewId}
 					           name={'Uncategorized'}
+					           stackField={stackField}
 					           table={this.props.table} />
 					{R.map(nameStack => <StackItem currentViewId={this.props.currentViewId}
+					                               key={nameStack + stackCounter++}
+					                               stackField={stackField}
 					                               name={nameStack}
 				                                   table={this.props.table}
 						/>)(fieldIdStacked ? stackField.fieldObj.options.select : [])}
--- frontend/src/javascripts/components/view/kanban/kanbanViewContainer/stackItem/records/recordItem.js
+++ frontend/src/javascripts/components/view/kanban/kanbanViewContainer/stackItem/records/recordItem.js
@@ -4,16 +4,35 @@ import R from 'ramda'
 import RecordCell from './recordCell/recordCell'
 import './recordItem.scss'
 
-const RecordItem = ({record, tableId, fields, currentView}) => {
+const RecordItem = ({record, tableId, fields, currentView, stackField, stackTitle}) => {
 	let counter = 0;
-	return (
-		<div className='record_item'>
+	const idx = R.path(['idx'], stackField)
+	const data = R.path(['record_data'], record)
+	const stackFieldValue = R.path([idx, 'data'], data)
+	if (stackTitle === stackFieldValue) {
+		return (
+			<div className='record_item'>
+				{R.map(data => <RecordCell key={data._id}
+				                           position={counter++}
+				                           fields={fields}
+				                           stackField={stackField}
+				                           data={data.data}/>)(record.record_data || [])}
+			</div>
+		)
+	}
+	if (stackFieldValue) {
+		return (<div/>)
+	}
+	if (stackField && stackTitle === 'Uncategorized') {
+		return (<div className='record_item'>
 			{R.map(data => <RecordCell key={data._id}
 			                           position={counter++}
 			                           fields={fields}
+			                           stackField={stackField}
 			                           data={data.data}/>)(record.record_data || [])}
-		</div>
-	)
+		</div>)
+	} else return (<div/>)
+
 }
 
 export default RecordItem
