f1c38611bd10b51e59635bc6247acfc28ac4e2ed
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-09-19 18:25:29 +0300

    disable btns in context menu

--- frontend/src/javascripts/components/contextMenu/contextMenuBaseOptions.js
+++ frontend/src/javascripts/components/contextMenu/contextMenuBaseOptions.js
@@ -6,16 +6,16 @@ let BaseOptions = (props) => {
   return (
     <div className = 'base-options-wrapper'>
       <ul className = 'base-options-list'>
-        <li className = 'base-options-list-item'>
+        <li className = 'base-options-list-item disabled'>
           <Icon name='share alternate square' className='icon' as = 'i' color='black' size='small'/>
           Share
         </li>
-        <li className = 'base-options-list-item'>
+        <li className = 'base-options-list-item disabled'>
           <Icon name='info circle' className='icon' color='black' size='small'/>
           Description
         </li>
         <ModalDublicateBase handleClick = {props.handleClick} teamId = {props.teamId} base = {props.base} teamNames={props.teamNames}/>
-        <li className = 'base-options-list-item'>
+        <li className = 'base-options-list-item disabled'>
           <Icon name='slack' className='icon' color='black' size='small'/>
           Slack notifications
         </li>
